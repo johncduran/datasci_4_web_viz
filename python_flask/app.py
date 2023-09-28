053844e65e91a99f9b6a4610b3d44ff55ab69c7d
johncduran <john.duran@stonybrook.edu>
2023-09-28 00:36:25 +0000

    added files to folders

--- python_flask/app.py
+++ python_flask/app.py
@@ -3,17 +3,43 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from io import BytesIO
 import base64
+import os
 
 app = Flask(__name__)
 
+url = "https://raw.githubusercontent.com/johncduran/datasci_4_web_viz/main/datasets/PLACES__Local_Data_for_Better_Health__County_Data_2023_release.csv"
+df = pd.read_csv(url)
+
+
+df = df[(df['MeasureId'] == 'DEPRESSION') & (df['Data_Value_Type'] == 'Age-adjusted prevalence')]
+
+grouped = df.groupby('LocationName').Data_Value.mean().sort_values(ascending=False)
+
+# Create an empty file if it does not exist
+if not os.path.exists("depression_per_location.png"):
+    with open("depression_per_location.png", "wb") as f:
+        f.write(b"")
+
+# Save the plot to the file
+plt.figure(figsize=(10, 7))
+grouped.plot(kind='bar', color='lightcoral')
+plt.ylabel('Average Data Value (Age-adjusted prevalence) - Percent')
+plt.xlabel('Location (County)')
+plt.title('Depression Age-adjusted Prevalence by County in HI')
+plt.xticks(rotation=90)
+plt.tight_layout()
+plt.savefig("depression_per_location.png")
+
 @app.route('/')
 def mainpage():
-    csv = pd.read_csv('https://raw.githubusercontent.com/johncduran/datasci_4_web_viz/main/datasets/PLACES__Local_Data_for_Better_Health__County_Data_2023_release.csv')
-    return render_template('base.html',csv=csv)
-
+    # Serve the image file to the user
+    with open("depression_per_location.png", "rb") as f:
+        image = f.read()
+    return render_template('base.html', image=image)
 
 if __name__ == '__main__':
     app.run(
         debug=True,
-        port=8000
-    )
+        port=8080,
+        host='0.0.0.0'
+)
